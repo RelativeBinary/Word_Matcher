0c98df13ac48bf264a3bea02b5e3b785cbc9ad09
Patrick Bornay <patrick.bornay@protonmail.com>
2019-08-24 19:42:50 +1000

    cleaned up code and made it compatible with g++ 4.2.2 because thats what they wanted...

--- ass1.cpp
+++ ass1.cpp
@@ -42,7 +42,7 @@ int Max(int a, int b){
 }
 
 int Height(Word_Type* node){
-    if(node == nullptr) return -1;
+    if(node == NULL) return -1;
     return node->height;
 }
 
@@ -50,8 +50,8 @@ int Height(Word_Type* node){
 Word_Type* NewNode(std::string word, bool isUnique, bool inDictionary){
     Word_Type* newWord_Type = new Word_Type();
     newWord_Type->word = word;
-    newWord_Type->left = nullptr;
-    newWord_Type->right = nullptr;
+    newWord_Type->left = NULL;
+    newWord_Type->right = NULL;
     newWord_Type->height = 0;
     newWord_Type->isUnique = isUnique;
     newWord_Type->inDic = inDictionary;
@@ -91,12 +91,12 @@ Word_Type* LeftRotate(Word_Type* nodeX){
 }  
 
 int GetBalance(Word_Type* node){
-    if(node == nullptr) return 0;
+    if(node == NULL) return 0;
     return Height(node->left) - Height(node->right);
 }
 
 Word_Type* Insert(Word_Type* root, std::string word, bool isUnique, bool inDictionary){
-    if(root == nullptr) {//empty tree
+    if(root == NULL) {//empty tree
         root = NewNode(word, isUnique, inDictionary);
     }
     else if (word <= root->word) {
@@ -144,7 +144,7 @@ Word_Type* Insert(Word_Type* root, std::string word, bool isUnique, bool inDicti
 
 bool Search(Word_Type* root, std::string word){
     //comparisons++;
-    if(root == nullptr){
+    if(root == NULL){
         return false;
     } else if (root->word == word){
         //this will only execute when the first duplicate of a word is searched
@@ -159,23 +159,23 @@ void InOrder(Word_Type* root){
     //left, data, right
 
     //left
-    if(root->left != nullptr) InOrder(root->left);
+    if(root->left != NULL) InOrder(root->left);
 
     //data
     std::cout << root->word << " ";
 
     //right
-    if(root->right != nullptr) InOrder(root->right);
+    if(root->right != NULL) InOrder(root->right);
 
     //exit
-    if(root->left == nullptr && root->right == nullptr) return;
+    if(root->left == NULL && root->right == NULL) return;
 }
 
 void InOrderUnique(Word_Type* root){
     //left, data, right
 
     //left
-    if(root->left != nullptr) InOrderUnique(root->left);
+    if(root->left != NULL) InOrderUnique(root->left);
 
     //data
     if (root->isUnique == true) {
@@ -183,17 +183,17 @@ void InOrderUnique(Word_Type* root){
     }
 
     //right
-    if(root->right != nullptr) InOrderUnique(root->right);
+    if(root->right != NULL) InOrderUnique(root->right);
 
     //exit
-    if(root->left == nullptr && root->right == nullptr) return;
+    if(root->left == NULL && root->right == NULL) return;
 }
 
 void Unique2Array(Word_Type* root, std::string* uwords_arr, int &end_uwords_arr) {
     //left, data, right
 
     //left
-    if(root->left != nullptr)
+    if(root->left != NULL)
         Unique2Array(root->left, uwords_arr, end_uwords_arr);
 
     //data
@@ -203,33 +203,38 @@ void Unique2Array(Word_Type* root, std::string* uwords_arr, int &end_uwords_arr)
     }
 
     //right
-    if(root->right != nullptr)
+    if(root->right != NULL)
         Unique2Array(root->right, uwords_arr, end_uwords_arr);
 
     //exit
-    if(root->left == nullptr && root->right == nullptr) return;
+    if(root->left == NULL && root->right == NULL) return;
 }
-
-
 #pragma endregion
 
 //STEP 4 FUNCTIONS
 #pragma region
+//swap function used in partition function
+void swap(char &a, char &b){
+    char temp = a;
+    a = b;
+    b = temp;
+}
+
 //partition functions used in quicksort
-int partition(char *arr, int ast, int aen){
+int partition(char* arr, int ast, int aen){
     int piv = arr[aen];
     int parIn = ast; //initialize start partition at the start of array
     for (int i = ast; i < aen; i++){
         if(arr[i] <= piv){ //move larger elements toward end of arr, move smaller elements toward start of arr
-            std::swap(arr[i], arr[parIn]);
+            swap(arr[i], arr[parIn]);
             parIn++;
         }
     }
-    std::swap(arr[parIn], arr[aen]);
+    swap(arr[parIn], arr[aen]);
     return parIn;
 }
 //quick sort function
-void qsort(char *arr, int ast, int aen){
+void qsort(char* arr, int ast, int aen){
     if (ast < aen){
         int parIn = partition(arr, ast, aen);
         qsort(arr, ast, parIn - 1);
@@ -240,10 +245,12 @@ void qsort(char *arr, int ast, int aen){
 void sort(std::string* word, int s, int e){
     char arr[35];
     int arr_end = 0;
-    for (char c : (*word)){
+    for (int i = 0; i < (*word).size(); i++){
+        char c = (*word)[i];
         arr[arr_end] = c;
         arr_end++;
     }
+
     qsort(arr, s, e);
     
     for (int i = 0; i < arr_end; i++){
@@ -255,6 +262,9 @@ void sort(std::string* word, int s, int e){
 
 //MAIN
 int main(){
+    //start whole program timer
+    clock_t main_time_begin = clock();
+
     //DICTIONARY INITIALIZATION
     #pragma region
         std::ifstream inword;
@@ -269,20 +279,23 @@ int main(){
         inword.close();
         std::cout << "Number of words in dictionary: " << end << '\n';
     #pragma endregion
-
+    //*
     //STEP 1 USE LINEAR SEARCH TO FIND FIRST 10 REVERSE WORDS IN DICTIONARY
     #pragma region
-    /*std::cout << "Find first 5 valid reversed words\n";
+    std::cout << "\nSTEP 1 - FIND 5 REVERSE PALINDROMES WITH LINEAR SEARCH\n";
     std::string rword;
     int matches = 0;
-    int comparisons = 0;
+    //int comparisons = 0;
+
+    //start time
     clock_t time_begin = clock();
+
     for (int i = 1; i <= end && matches < 5; i++){
         if (dic[i].size() > 1){
             rword = std::string(dic[i].rbegin(), dic[i].rend());
             //find word with linear search
             for (int j = 1; j <= end; j++){
-                comparisons++;
+                //comparisons++;
                 if ((dic[j] == rword)) {
                     std::cout << dic[i] << " : " << rword << std::endl;
                     matches++;
@@ -291,22 +304,27 @@ int main(){
             }
         }
     }
+
+    //end time
     clock_t time_end = clock();
     double elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
-    std::cout << "comparisons: " << comparisons << std::endl;
+
+    //std::cout << "comparisons: " << comparisons << std::endl;
     std::cout << "time (sec): " << elapsed_secs << std::endl;
-    std::cout << std::endl;*/
+    std::cout << std::endl;//*/
     #pragma endregion
 
     //STEP 2 USE BINARY SEARCH TO FIND ALL REVERSE WORDS IN DICTIONARY
     #pragma region
-    /*int lower, higher, mid;
+    //*
+    std::cout << "\nSTEP 2 - FIND ALL PALINDROMES" << std::endl;
+    int lower, higher, mid;
     //int matches = 0;
-    std::string rword = "";
+    rword = "";
     bool found;
-    int comparisons = 0;
+    //comparisons = 0;
     //start timing
-    clock_t time_begin = clock();
+    time_begin = clock();
     for (int i = 1; i <= end; i++){
         if (dic[i].size() > 1){
             rword = std::string(dic[i].rbegin(), dic[i].rend());
@@ -316,12 +334,12 @@ int main(){
             found = false;
             while(!found && lower <= higher){
                 mid = (lower + higher) / 2;
-                comparisons++;
+                //comparisons++;
                 if(dic[mid] == rword) {
                     found = true;
                     //matches++;
                 } else {
-                    comparisons++;
+                    //comparisons++;
                     if (dic[mid] > rword) {
                         higher = mid -1;
                     } else {
@@ -332,17 +350,17 @@ int main(){
         }
     }
     //end timing
-    clock_t time_end = clock();
-    double elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
-    std::cout << "comparisons: " << comparisons << std::endl;
+    time_end = clock();
+    elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
+    //std::cout << "comparisons: " << comparisons << std::endl;
     std::cout << "time for binary to find ALL (sec): " << elapsed_secs << std::endl;
-    std::cout << std::endl;*/
+    std::cout << std::endl;//*/
     #pragma endregion
     
     //STEP 2 FIND 10 AND LARGEST REVERSE WORD
     #pragma region
-    /*
-    std::cout << "Find first 10 valid reversed words and largest reversed word, with binary search.\n";
+    //*
+    std::cout << "\nSTEP 2 - FIND 10 PALINDROMES AND LARGEST PALINDROME" << std::endl;
     matches = 0;
     int largest = 2;
     int lindex;
@@ -384,35 +402,37 @@ int main(){
     time_end = clock();
     elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
     std::cout << "time to find first 10 and largest (sec): " << elapsed_secs << std::endl;
-    std::cout << std::endl;*/
+    std::cout << std::endl;//*/
 
     //STEP 2 Find All/Find 10 and largest, SPEED IMPROVMENTS:
     /*  Previous version of the algorithm, would take a word at index i from dictionary array and assign it to a variable 'word' to be used in the algorithm
         Removing that assignment and just refering to dic[i] throughout the algorithm improved speed.
         Before --> After
-        5.38s  --> 5.05s
+        ~5.38s  --> ~4.15s
 
         Adding a condition to check if the word is already a palindrome before searching the dictionary cause time to complete to increase.
         Before --> After
-        5.38s  --> 7.88s
+        ~5.38s  --> ~7.88s
     */
    #pragma endregion
 
     //STEP 3 SAMPLE TEXT SPELL CHECKER
     #pragma region
-
+    std::cout << "\nSTEP 3 SPELL CHECKER" << std::endl;
     //Read the input sample.txt file
     inword.open("sample.txt");
     if (!inword.is_open()){ return -1;}
     std::string nword;
     Word_Type* rootptr;
-    rootptr = nullptr;
+    rootptr = NULL;
+
+    //start timing
+    time_begin = clock();
 
     //begin spell checking
     while (inword >> nword){
         //preprocess word
         clean(nword);
-        //std::cout << nword << " ";
         //check if valid word
         if (nword.size() > 0){
             Word_Type::vwords++;
@@ -430,6 +450,7 @@ int main(){
                 //search in dictionary to check spelling via binary search
                 int lower = 1, higher = end, mid;
                 bool found = false;
+                //binary search
                 while(!found && lower <= higher){
                     mid = (lower + higher) / 2;
                     if(dic[mid] == nword) {
@@ -446,7 +467,6 @@ int main(){
                 if(isUnique) Word_Type::uwords++;
                 if(isUnique && inDictionary) Word_Type::udwords++;
                 //add to words tree, AVL tree will balance itself
-                //std::cout <<"Insertion call: "<< nword << " || uwords count: " << Word_Type::uwords << " || udwords count: " << Word_Type::udwords << std::endl;
                 rootptr = Insert(rootptr, nword, true, inDictionary);
             }  
         }
@@ -456,7 +476,12 @@ int main(){
     std::cout << "Number of valid words read: " << Word_Type::vwords << std::endl;
     std::cout << "Number of unique words read: " << Word_Type::uwords << std::endl;
     std::cout << "Number of unique dictionary words read: " << Word_Type::udwords << std::endl;
-    
+
+    //end timing
+    time_end = clock();
+    elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
+    std::cout << "time to run step 3 (sec): " << elapsed_secs << std::endl;
+
     //storing unique words in a suitable array.
     std::string uwords_arr[500];
     int end_uwords_arr = 0;
@@ -470,26 +495,21 @@ int main(){
     std::cout << "Displaying all the unique words found in the file: " << std::endl;
     std::cout << std::endl;
     InOrderUnique(rootptr);
-    //END OF COMMENT BLOCK*/
-
-    //pre process the words read, by converting to lowercase with no punchtuations (as one word), 0 char words are rejected from the sample.txt
-
-    //STRAT 3 (with structs): get word, preprocess, if valid increment vwords, search in WORDSARR if found && .isUnique == true then set to false and decrement unique words (if .inDictionary was also true then decrement udwords as well) then break otherwise just break, 
-    
-    //if not found search in dictionary if found in dictionary add to end of WORDSARR increment uwords and udwords and set .inDictionary to true and insertion sort it, if not found in dictionary add to WORDSARR increment uwords and set .inDictionary to false and insertion sort it.
-
-    //print, num valid words, num unique words, num of unique that were in dictionary
+    //*/
     #pragma endregion
 
-//Step 4 - Anagrams
-/*
-    //STRAT 1: for each word in unique_words array, create search and sort permutatioins, if valid print, increment amagram_words and increment anagrams_found, keep count of word which had most anagrams and how many and its index in uniquewords array, repeat for next permutation, repeat for next word, be sure to do more than a binary search
-*/
+//STEP 4 - ANAGRAMS
+    std::cout << "\nSTEP 4 ANAGRAMS" << std::endl;
+
     std::string anagrams[500];
-    int end_anagrams = 0, max_anagrams = 0, total_anagrams = 0;
-    int s4_matches = 0, max_anagram_index, anagram_words = 0, anagrams_found= 0;
+    int end_anagrams = 0, s4_matches = 0;
+    int max_anagrams = 0, max_anagram_index, anagram_words = 0, anagrams_found = 0;
     int largest_anagram_index, largest_anagram_size = 0;
     std::string largest_anagram_sample = "";
+
+    //start timing
+    time_begin = clock();
+
     for (int i = 1; i < end_uwords_arr; i++){
         //create sorted alternative (potential optimization check if word is more than 1 character long before doing all this)
         std::string sort_origin = uwords_arr[i];
@@ -539,31 +559,40 @@ int main(){
         }
         end_anagrams = 0;
     }
+    std::cout << std::endl;
     std::cout << "Word that had the most anagrams: " << uwords_arr[max_anagram_index] << " => " << max_anagrams << " anagrams.\n";
     std::cout << "Largest valid word that has an anagram: " << uwords_arr[largest_anagram_index] /*<<" => " << largest_anagram_sample*/ << std::endl;
     std::cout << "Total amount of words with anagrams: " << anagram_words << std::endl;
     std::cout << "Total anagrams found: " << anagrams_found << std::endl;
- 
+
+    //end timing
+    time_end = clock();
+    elapsed_secs = double(time_end - time_begin) / CLOCKS_PER_SEC;
+    std::cout << "time to run step 4 (sec): " << elapsed_secs << std::endl;
+
+    //main end timing
+    clock_t main_time_end = clock();
+    double main_elapsed_secs = double(main_time_end - main_time_begin) / CLOCKS_PER_SEC;
+    std::cout << "time to run main (sec): " << main_elapsed_secs << std::endl;
 }
 
 
 /*******************************************************************//*
                                 STEP 5
+STEP 1/2 SPEED UP NOTES
 Step 1 linear search for first 10 emordnilap time   ==> 57.23sec ~ 1min
-Step 2 linear search for all emordnilap estimation - 10th emordnilap 
-is the 616th word which is 0.16644% of the total dictionary. 
-0.16644 * 600.8165584 ~ 100%
-~1min * 600.8165584 = 600.8165584mins ~ 10hrs
-binary search too ~5.05s to find all reverse palindromes.
-(5.05/600.8165584mins * 60s) * 100 = 0.014% of linear search's time
+Step 2 linear search for all emordnilap estimation - 10th emordnilap is the 616th word which is 0.16644% of the total dictionary. 
+0.16644 * 600.8165584 ~ 100%, ~1min * 600.8165584 = 600.8165584mins ~ 10hrs
+binary search too ~5.05s to find all reverse palindromes. (5.05/600.8165584mins * 60s) * 100 = 0.014% of linear search's time
 99.986% reduction in time taken. Based on linear estimation
-
 Step 2 binary search for all emordnilap time taken  ==> 5.05
 Step 2 speedup factor achieved                      ==> 99.986%
-Run time of whole program                           ==> ???
-Machine                                             ==> BANSHEE SERVER
+
+Run time of whole program       ==> ???
+Machine                         ==> UOW BANSHEE SERVER
 DS and Algos used in: 
-spell checker                                       ==> my own algorithm
-find emordnilap                                     ==> my own algorithm
-find anagrams                                       ==> match sorted strings algorithm
+spell checker                   ==> AVL Data Structure & Algorithm from Geeks4Geeks tutorials
+find reverse palindrome         ==> my own algorithm
+find anagrams                   ==> quicksort tutorial from mycodeschool
+OPTIMISATIONS: improved binary search by using direct access to dictionary array rather than storing string (see end of step 2 for details). for spell checker I replaced the standard array for storing unique words and used an AVL BTree to reduce the comparisons needed to check if a word is already in the unique words array and to reduce swaps and comparisons for sorting a standard array after adding a new word to unique words. For anagrams used the 2nd algorithm for finding anagrams in the lectures, then i switched to an algorithm which sorts the unique word and every word checked against it, if they match then its an anagram. This removed the need to have a process which counted the amount of times a letter appeared and each letter that appeard for the unique word, and for the dictionary word it was being compared against. The sort algorithm used was quick sort, i also realised that i didnt need to run a sort over the array which held the current unique word's anagrams as they were added to the temporary array in alphabetic order already since the dictionary was already in alphabetic order. 
 *//******************************************************************/
